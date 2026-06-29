5b1448e5b7c294c81349a860a29cabace0c04663
Mahamat Nour BRAHIM <mahamatnourbrahim235@gmail.com>
2026-06-29 10:30:41 +0000

    initialisation des deux projets Spring Boot

---
+++ badwallet-api/src/test/java/com/examen/badwallet_api/BadwalletApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.examen.badwallet_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BadwalletApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
